61d4e5179173cf4b8c3d43cdd159e926a53247e7
sal400 <flarebd@gmail.com>
2023-10-20 04:14:01 +0600

    Database connected to the server

--- index.js
+++ index.js
@@ -7,10 +7,50 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.rkpusfk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    //Users code start from here:
+    const database = client.db('nestro')
+    const brands = database.collection('brands')
+    const products = database.collection('products')
+    
+    app.get('/brands', async(req, res)=>{
+        const result = await brands.find().toArray()
+        res.send(result)
+    })
+    
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res)=>{
-    res.send('Welcome to nest-server')
+    res.send('Welcome to nestro-server')
 })
 
 app.listen(port, ()=>{
-    console.log(`nest-server is running on ${port}`)
+    console.log(`nestro-server is running on ${port}`)
 })
